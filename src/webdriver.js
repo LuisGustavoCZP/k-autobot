5f70a2f5dcd3bd1c17a21f5d4541f5b646ea1337
Luis Gustavo Cardoso Zanetti Pereira <luisgustavoczp@gmail.com>
2022-08-11 16:29:39 +0000

    update

--- src/webdriver.js
+++ src/webdriver.js
@@ -1,8 +1,12 @@
-const {Builder, Browser, By, Key, until, WebDriver, WebElement, WebElementPromise, WebElementCondition} = require('selenium-webdriver');
-
+const {Builder, Browser, By, Key, until, WebDriver, WebElement, WebElementPromise, WebElementCondition, Options} = require('selenium-webdriver');
+const { ServiceBuilder } = require('selenium-webdriver/firefox');
 async function createDriver (url)
 {
     let driver = await new Builder().forBrowser(Browser.FIREFOX).build();
+    const serviceBuilder = new ServiceBuilder("");
+    const options = new Options(driver);
+    options.b = '/var/lib/flatpak/app/org.mozilla.firefox/x86_64/stable/9c4d1797620e52ab380ffcbbff9a947ccb39695a8ca14e44935d9578d5c88b6d/files/lib/firefox/';
+    
     await driver.get(url);
     return driver;
 }
